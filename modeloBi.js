37670636e59561e775f91f9970390d206e58ff43
DevJFI <suporte@jfi.com.br>
2025-03-25 10:42:09 -0300

    Update modeloBi.js

--- modeloBi.js
+++ modeloBi.js
@@ -1,9 +1,11 @@
 function ExibeBI(link){
-  var html='<html>'+
+  var html=
+/*    '<html>'+
 '<head>'+
 '	<title></title>'+
 '	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>'+
-'	<style type="text/css">.main iframe {'+
+'	<style type="text/css">'+
+'    .main iframe {'+
 '		   border: none;'+
 '		   position: fixed;'+
 '		   top: 0;'+
@@ -18,12 +20,8 @@ function ExibeBI(link){
 '		}'+
 '	</style>'+
 '</head>'+
-'<body class="vsc-initialized">'+
-'<div class="main"><iframe allowfullscreen="true" frameborder="0" height="373.5" src="'+link+'"></iframe><img alt="" data-units="px" id="imgpos" src="https://lh3.googleusercontent.com/drive-viewer/AJc5JmQofLcpxnwsEZz9GeGoG7fOWMIq8hW69wbB7XVscYFdNCeNeB0TkbmAkwSPIwNur2dT-ecexTk=w1366-h625" /></div>'+
-'</body>'+
-'</html>'
-  /*var classeDocumento =  document.getElementsByClassName('WCMHtmlEditor')
-  var divExibicao = classeDocumento[0].id*/
+'<body class="vsc-initialized">'+*/
+'<div class="main"><iframe allowfullscreen="true" frameborder="0" height="373.5" src="'+link+'"></iframe><img alt="" data-units="px" id="imgpos" src="https://lh3.googleusercontent.com/drive-viewer/AJc5JmQofLcpxnwsEZz9GeGoG7fOWMIq8hW69wbB7XVscYFdNCeNeB0TkbmAkwSPIwNur2dT-ecexTk=w1366-h625" /></div>'
   var divBI = document.getElementById('BI')
   alert(html)
   divBI.innerHtml = html
