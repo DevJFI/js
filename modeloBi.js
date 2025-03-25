ca1a15b67ad1c1e93ec7a6c3b05ab7f7e0a3fddc
DevJFI <suporte@jfi.com.br>
2025-03-25 11:13:09 -0300

    Update modeloBi.js

--- modeloBi.js
+++ modeloBi.js
@@ -1,6 +1,7 @@
 function ExibeBI(link){
   var html='<div class="main"><iframe allowfullscreen="true" frameborder="0" height="373.5" src="'+link+'"></iframe><img alt="" data-units="px" id="imgpos" src="https://lh3.googleusercontent.com/drive-viewer/AJc5JmQofLcpxnwsEZz9GeGoG7fOWMIq8hW69wbB7XVscYFdNCeNeB0TkbmAkwSPIwNur2dT-ecexTk=w1366-h625" /></div>'
-  var divBI = document.getElementById('BI')
+  //var divBI = document.getElementById('BI')
+  var divBI = $('#BI')
   alert(html)
   divBI.innerHtml = html
   
