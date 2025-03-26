0cd11a9901b5175e7d7d46e00ad5708c2f80fbfe
DevJFI <suporte@jfi.com.br>
2025-03-26 08:48:02 -0300

    Delete modeloBi1.js

--- modeloBi1.js
+++
@@ -1,4 +0,0 @@
-function ExibeBI(link){
-  var html='<div class="main"><iframe allowfullscreen="true" frameborder="0" height="373.5" src="'+link+'"></iframe><img alt="" data-units="px" id="imgpos" src="https://lh3.googleusercontent.com/drive-viewer/AJc5JmQofLcpxnwsEZz9GeGoG7fOWMIq8hW69wbB7XVscYFdNCeNeB0TkbmAkwSPIwNur2dT-ecexTk=w1366-h625" /></div>'
-  return html
-}
